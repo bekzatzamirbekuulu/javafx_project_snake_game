61305b1954ded72bb9c3db5aeea184a960955d41
bekzatzamirbekuulu <73532500+bekzatzamirbekuulu@users.noreply.github.com>
2021-03-20 21:27:34 +0600

    Update Controller.java

--- Controller.java
+++ Controller.java
@@ -9,9 +9,9 @@ import java.util.Random;
 public class Controller extends JPanel implements ActionListener {
     static final int SCREEN_WIDTH = 700;
     static final int SCREEN_HEIGHT = 600;
-    static final int UNIT_SIZE = 25;
+    static final int UNIT_SIZE = 30;
     static final int GAME_UNITS = (SCREEN_WIDTH*SCREEN_HEIGHT)/UNIT_SIZE;
-    static final int DELAY = 75;
+    static final int DELAY = 70;
     final int x[] = new int[GAME_UNITS];
     final int y[] = new int[GAME_UNITS];
     int body = 6;
