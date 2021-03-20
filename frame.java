1e857a7bb6b6675a93baec01c6d73bf61352bd62
bekzatzamirbekuulu <73532500+bekzatzamirbekuulu@users.noreply.github.com>
2021-03-20 21:21:44 +0600

    Add files via upload

---
+++ frame.java
@@ -0,0 +1,15 @@
+package sample;
+
+import javax.swing.*;
+
+public class frame extends JFrame {
+    frame(){
+        this.add(new Controller());
+        this.setTitle("Snake");
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setResizable(false);
+        this.pack();
+        this.setVisible(true);
+        this.setLocationRelativeTo(null);
+    }
+}
